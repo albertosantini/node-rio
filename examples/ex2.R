b8414101a8c714b8c4d5e8aa12a12d2ead2c63cb
icebox <albertosantini@gmail.com>
2011-08-07 00:54:35 +0200

    Added example 2, sourcing and calling a script with a json object.

---
+++ examples/ex2.R
@@ -0,0 +1,48 @@
+require(tseries)
+require(RJSONIO)
+
+hackZeroes <- function (x) {
+    for (i in which(x == 0)) {
+        if (i == 1) {
+            c = i
+            while (x[c] == 0) {
+                c = c + 1
+            }
+            x[i] <- x[c]
+        } else {
+            x[i] <- x[i - 1]
+        }
+    }
+    x
+}
+
+getReturns <- function (symbol) {
+    assetPrice <- coredata(get.hist.quote(symbol,
+        start="2009-8-6", end="2011-8-6",
+        compression="w", quote="Close", quiet=TRUE))
+    assetPrice <- hackZeroes(assetPrice)
+    assetReturns <- diff(log(assetPrice[1:(length(assetPrice)-1)]))
+
+    return(assetReturns)
+}
+
+getOptimalPortfolio <- function (jsonObj) {
+    x <- c()
+
+    o = fromJSON(jsonObj)
+    symbols <- o$prods
+
+    for (asset in symbols) {
+        rets = getReturns(asset);
+        x <- cbind(x, rets)
+    }
+
+    # Target Return (pm) is weekly
+    res <- portfolio.optim(x, pm = mean(x),
+        reslow = c(0, 0, 0), reshigh = c(1, 1, 1))
+
+    return(toJSON(res$pw))
+}
+
+# args <- '{"prods":["IBM","YHOO","MSFT"]}';
+# print(getOptimalPortfolio(args))
---
+++ examples/ex2.js
@@ -0,0 +1,19 @@
+var sys = require('sys'),
+    rio = require('rio');
+
+function displayResponse(res) {
+    sys.puts("Optimal weights: " + res);
+}
+
+var args = {
+    prods: ["IBM", "YHOO", "MSFT"]
+};
+
+var jsonArgsString = JSON.stringify(args);
+
+// Please, change the path of the script based on Rserve machine
+rio.Rserve_eval("setwd('/My/Dev/node-rio/examples')", function () {});
+rio.Rserve_eval("source(file='ex2.R', skip.echo=1); " +
+    "getOptimalPortfolio('" + jsonArgsString + "')",
+    displayResponse);
+
