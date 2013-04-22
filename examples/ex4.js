2d89110a6fccfe65274aaa4dbdd30d949a53e072
icebox <albertosantini@gmail.com>
2013-04-22 18:57:44 +0200

    Fix for utf8 chars in files or strings.
    Jslint refactor.

---
+++ examples/ex4.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var rio = require("../lib/rio");
+
+rio.evaluate("# ห\n2+2");
+rio.evaluate("# แผ่นดินฮั่นเสื่อมโทรมแสนสังเวช  พระปกเกศกองบู๊กู้ขึ้นใหม่\n65+4");
+rio.evaluate("# แผ่องบู๊กู้ขึ้นใหม่\n2*3");
