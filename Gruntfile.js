b9ec269f9986ea20eb89511d0441a55df173b79c
icebox <albertosantini@gmail.com>
2014-01-18 00:59:10 +0100

    Fix lint error.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
         jshint: {
             options: {
-                jshintrc: ".jshintrc",
+                jshintrc: ".jshintrc"
             },
             src: [
                 "lib/**/*.js",
