56372faa36a998188dce5a420c0bd2ede38f26f4
Alex Proca <alex.proca@gmail.com>
2014-11-25 23:00:24 +0200

    Added option to call functions already loaded in R session

---
+++ examples/ex6.js
@@ -0,0 +1,18 @@
+// In order to start server run following line in examples directory
+// R -f server.R --gui-none --no-save
+
+var rio = require("../lib/rio");
+
+function printEcho(err, res) {
+    if (err) {
+        console.log('An error occured : ' + err)
+    } else {
+        console.log("Response is : " + res);
+    }
+}
+
+rio.bufferAndEval(null, {
+    entryPoint: "echo",
+    data: ["test", "data"],
+    callback: printEcho
+});
