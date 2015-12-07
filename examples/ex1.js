461cedb3863cba8c6f42e2523c6ab7c6a3a4fd52
icebox <albertosantini@gmail.com>
2015-12-07 12:49:33 +0100

    Fix linter errors in example 1.

--- examples/ex1.js
+++ examples/ex1.js
@@ -3,8 +3,8 @@
 var rio = require("../lib/rio");
 
 // rio.enableDebug(true);
-// rio.enableRecordMode(true, {fileName: 'dump.bin'});
-// rio.enablePlaybackMode(true, {fileName: 'dump.bin'});
+// rio.enableRecordMode(true, {fileName: "dump.bin"});
+// rio.enablePlaybackMode(true, {fileName: "dump.bin"});
 
 rio.e({command: "pi / 2 * 2"});
 rio.e({command: "c(1, 2)"});
